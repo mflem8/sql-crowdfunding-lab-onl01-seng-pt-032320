d07aecb9a215426499e7748779d5d07c1b352bfe
mflem8 <matthewfleming8@gmail.com>
2020-05-26 14:38:01 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,13 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "Write your SQL query Here"
+  "SELECT projects.title, pledges.amount FROM projects INNER JOIN
+   pledges ON projects.id = pledges.id ORDER BY title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "Write your SQL query Here"
+  "SELECT users.name, users.age, pledges.amount FROM users INNER JOIN pledges ON
+   users.id = pledges.id ORDER BY name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
